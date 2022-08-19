1d7ed4e24f658f5d7c2400345e8398e37b21ad29
Lilay <Lilay4618@mail.ru>
2022-08-19 20:05:34 +0500

    Home work 3

--- Work112/Program.cs
+++ Work112/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Задайте координаты точек в формате (х1, х2) (y1, y2)");
+﻿Console.WriteLine("Задайте координаты точек в формате (х1, y1, z1) (x2, y2, z2)");
 Console.Write("Введите значение х1 ");
 double x1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите значение х2 ");
@@ -7,8 +7,12 @@ Console.Write("Введите значение y1 ");
 double y1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите значение y2 ");
 double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение z1 ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение z2 ");
+double z2 = Convert.ToDouble(Console.ReadLine());
 
 double distance = 0;
 
-distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
 Console.WriteLine($"Расстояние между заданными координатами = {distance}");
