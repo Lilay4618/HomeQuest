b601f88a3010699baff4524107326ae86e4477cb
Lilay <Lilay4618@mail.ru>
2022-08-29 21:19:40 +0500

    Домашка-6

---
+++ Work6.1/Program.cs
@@ -0,0 +1,44 @@
+﻿void numberArr(int[] arr)
+{
+    int number = arr.Length;
+    int index = 0; 
+    Console.Write("Из чисел: ");
+    while(index < number)
+    {
+        arr[index] = new Random().Next(-99999, 100000);
+        Console.Write($"[{arr[index]}] ");
+        index++;
+    }
+}
+
+int notSubZero(int[] arr1)
+{
+    int index = arr1.Length;
+    int count = 0;
+    int result = 0;
+    while(count < index)
+    {
+        if(arr1[count] > 0)
+        {
+            result++;
+        }
+        count++;
+    }
+    return result;
+}
+
+Console.Write("Введите количество чисел для проверки: ");
+string quan = Console.ReadLine() ?? "";
+int quanNum;
+
+if(int.TryParse(quan, out quanNum));
+else
+{
+    Console.WriteLine("Ошибка! Введите числовое значение!");
+}
+
+int[] array = new int[quanNum];
+numberArr(array);
+Console.WriteLine();
+int final = notSubZero(array);
+Console.WriteLine($"Количество положительных: {final}");
---
+++ Work6.2/Program.cs
@@ -0,0 +1,57 @@
+﻿double findX(double b1, double b2, double k1, double k2)
+{
+    double numX = (b2 - b1) / (k1 - k2); 
+    return numX;
+}
+
+void findY(double numberX, double b1, double b2, double k1, double k2)
+{
+    double numY1 = k1 * numberX + b1;
+    double numY2 = k2 * numberX + b2;
+    if(numY1 == numY2)
+    {
+        Console.WriteLine($"Координаты точки пересечения: ({numberX};{numY1})");
+    } 
+    else
+    {
+        Console.WriteLine("Ошибка в программе");
+    }
+}
+
+
+double numK1;
+double numK2;
+double numB1;
+double numB2;
+
+Console.WriteLine("Чтобы найти точку пересечения двух прямых, заданных уравнениями y=k1*x+b1, y=k2*x*b2:");
+Console.WriteLine("Введите значение k1: ");
+string k1 = Console.ReadLine() ?? "";
+
+if(double.TryParse(k1, out numK1))
+{
+    Console.WriteLine("Введите значение k2: ");
+    string k2 = Console.ReadLine() ?? "";
+    if(double.TryParse(k2, out numK2))
+    {
+        Console.WriteLine("Введите значение b1: ");
+        string b1 = Console.ReadLine() ?? "";
+        if(double.TryParse(b1, out numB1))
+        {
+            Console.WriteLine("Введите значение b2: ");
+            string b2 = Console.ReadLine() ?? "";
+            if(double.TryParse(b2, out numB2))
+            {
+                double numX = findX(numB1, numB2, numK1, numK2);
+                findY(numX, numB1, numB2, numK1, numK2);
+            }
+            else Console.WriteLine("Ошибка! Заданные значения должны быть числовыми");
+        }
+        else Console.WriteLine("Ошибка! Заданные значения должны быть числовыми");
+    }
+    else Console.WriteLine("Ошибка! Заданные значения должны быть числовыми");
+}
+else Console.WriteLine("Ошибка! Заданные значения должны быть числовыми");
+    
+
+
