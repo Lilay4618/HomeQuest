e4fc0285bc6892ab8c7863a20dad110f5700b416
Lilay <Lilay4618@mail.ru>
2022-09-02 22:47:32 +0500

    Домашка №7

---
+++ Work7.1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите количество строк массива");
+string str = Console.ReadLine() ?? "";
+Console.WriteLine("Введите количество столбцов массива");
+string sto = Console.ReadLine() ?? "";
+int numStr;
+int numSto;
+int.TryParse(str, out numStr);
+int.TryParse(sto, out numSto);
+
+int[,] a = new int[numStr, numSto];
+ 
+Random random = new Random();
+for (int i = 0; i < numStr; i++)
+{
+    for (int j = 0; j < numSto; j++)
+    {
+        a[i, j] = random.Next(100);
+        Console.Write("{0,4}", a[i, j]);
+    }
+    Console.WriteLine();
+}
+
